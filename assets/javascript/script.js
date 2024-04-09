f504114973054373d964d3979c0e7f85e6a0e817
Magda Dmitruk <dmitrukm@gmail.com>
2024-04-09 04:02:59 +0000

    added variables to script.js

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+/* Declared variables */
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userScoreDisplay = document.getElementById('player-score');
+let computerScoreDisplay = document.getElementById('computer-score');
+let userChoice
+let computerChoice
+let userScore = 0;
+let computerScore = 0;
+let result
+
